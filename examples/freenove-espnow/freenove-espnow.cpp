dc3a8b5eb16b28be94882faa5fbe83c827757b2a
Hpsaturn <hpsaturn@gmail.com>
2024-01-25 08:41:31 +0100

    testing bytes data proto type (not func method)

--- examples/freenove-espnow/freenove-espnow.cpp
+++ examples/freenove-espnow/freenove-espnow.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
+#include <esp_now.h>
+#include <pb_encode.h>
 #include "CamFreenove.h"
+#include "frame.pb.h"
 
-// #define CONVERT_TO_JPEG
+#define CONVERT_TO_JPEG
 
 CamFreenove Camera;
+
+/// general buffer for msg sender
+uint8_t send_buffer[256];
 
 void setup() {
   Serial.begin(115200);
@@ -20,6 +26,10 @@ void setup() {
     size_t psram_size = esp_spiram_get_size() / 1048576;
     Serial.printf("PSRAM size: %dMb\r\n", psram_size);
   }
+
+  if (esp_now_init() == ESP_OK) {
+    Serial.println("ESPNow Init Success");
+  }
 }
 
 uint16_t frame = 0;
@@ -34,6 +44,55 @@ void printFPS(const char *msg) {
   } 
 }
 
+size_t encodeMsg(Frame msg) {
+  pb_ostream_t stream = pb_ostream_from_buffer(send_buffer, sizeof(send_buffer));
+  bool status = pb_encode(&stream, Frame_fields, &msg);
+  size_t message_length = stream.bytes_written;
+  if (!status) {
+    printf("Encoding failed: %s\r\n", PB_GET_ERROR(&stream));
+    return 0;
+  }
+  return message_length;
+}
+
+bool sendMessage(uint32_t msglen, const uint8_t *mac) {
+  esp_now_peer_info_t peerInfo = {};
+  memcpy(&peerInfo.peer_addr, mac, 6);
+  if (!esp_now_is_peer_exist(mac)) {
+    esp_now_add_peer(&peerInfo);
+  }
+  esp_err_t result = esp_now_send(mac, send_buffer, msglen);
+
+  if (result == ESP_OK) {
+    // Serial.println("send msg success");
+    return true;
+  } else if (result == ESP_ERR_ESPNOW_NOT_INIT) {
+    Serial.println("ESPNOW not Init.");
+  } else if (result == ESP_ERR_ESPNOW_ARG) {
+    Serial.println("Invalid Argument");
+  } else if (result == ESP_ERR_ESPNOW_INTERNAL) {
+    Serial.println("Internal Error");
+  } else if (result == ESP_ERR_ESPNOW_NO_MEM) {
+    Serial.println("ESP_ERR_ESPNOW_NO_MEM");
+  } else if (result == ESP_ERR_ESPNOW_NOT_FOUND) {
+    Serial.println("Peer not found.");
+  } else {
+    Serial.println("Unknown error");
+  }
+  return false;
+}
+
+void dispatchFrame(uint8_t *data, uint32_t data_len) {
+    Frame msg = Frame_init_zero;
+    msg.chunk_left = data_len;
+    msg.chunk_max = data_len;
+    msg.lenght = 220;
+    uint8_t chunk [220];
+    memcpy(&chunk, data, 220);
+    msg.data.arg = &chunk;
+    // msg.data.funcs.encode = Frame
+}
+
 void loop() {
   if (Camera.get()) {
 #ifdef CONVERT_TO_JPEG
@@ -41,6 +100,7 @@ void loop() {
     size_t out_jpg_len = 0;
     frame2jpg(Camera.fb, 64, &out_jpg, &out_jpg_len);
     printFPS("JPG compression at");
+    dispatchFrame(out_jpg, out_jpg_len);
     // Display.drawJpg(out_jpg, out_jpg_len, 0, 0, dw, dh);
     free(out_jpg);
 #else
