f0f51ce91973e96e88cd637d93d50ef6fa46bf45
Hpsaturn <hpsaturn@gmail.com>
2024-11-25 23:03:10 +0100

    v0.1.16 Elecrow Crowpanel receiver sample and some minors

--- src/ESPNowCam.h
+++ src/ESPNowCam.h
@@ -13,8 +13,8 @@ extern "C" {
 typedef void (*RecvCb)(uint32_t lenght);
 }
 
-#define ENC_VERSION "0.1.15"
-#define ENC_REVISION 080
+#define ENC_VERSION "0.1.16"
+#define ENC_REVISION 081
 
 class ESPNowCam {
  private:
