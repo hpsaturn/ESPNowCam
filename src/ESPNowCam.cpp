e3724d9be343ad8bf1b9b732eff2bded1c36366e
Hpsaturn <hpsaturn@gmail.com>
2025-03-09 12:10:43 +0100

    initial proposal to set the WiFi channel before init

--- src/ESPNowCam.cpp
+++ src/ESPNowCam.cpp
@@ -262,6 +262,9 @@ void ESPNowCam::setRecvFilter(uint8_t *fb, const uint8_t *macAddr, RecvCb cb) {
 /***********************************
  * C O M M O N  S E C T I O N
 ************************************/
+void ESPNowCam::setChannel(uint8_t channel) {
+  channel = channel;
+}
 
 bool ESPNowCam::init(uint8_t chunk_size) {
   chunksize = chunk_size; 
@@ -272,6 +275,8 @@ bool ESPNowCam::init(uint8_t chunk_size) {
   // shutdown wifi
   WiFi.disconnect();
   delay(100);
+
+  esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
 
   if (esp_now_init() == ESP_OK) {
     log_i("ESPNow Init Success");
