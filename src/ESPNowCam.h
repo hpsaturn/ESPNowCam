89af3cf1db847676e2a3ab13a3dafe670b769cfe
Hpsaturn <hpsaturn@gmail.com>
2025-04-05 13:22:29 +0200

    Added custom channel handling

--- src/ESPNowCam.h
+++ src/ESPNowCam.h
@@ -2,6 +2,7 @@
 #define ESPNOWCAM_H
 
 #include <WiFi.h>
+#include <esp_wifi.h>
 #include <esp_now.h>
 #include <pb_decode.h>
 #include <pb_encode.h>
@@ -19,10 +20,12 @@ typedef void (*RecvCb)(uint32_t lenght);
 class ESPNowCam {
  private:
   uint8_t targetAddress[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+  uint8_t channel = -1;
  public:
   void setRecvCallback(RecvCb cb);
   void setRecvBuffer(uint8_t *fb);
   void setRecvFilter(uint8_t *fb, const uint8_t *macAddr, RecvCb cb);
+  void setChannel(uint8_t channel);
   bool sendData(uint8_t* data, uint32_t lenght);
   bool setTarget(const uint8_t* macAddress);
   bool init(uint8_t chunksize = 244);
