7f1d9bce3f1f8cea62aee8756586bd8f09df2ce3
Hpsaturn <hpsaturn@gmail.com>
2026-03-08 19:01:14 +0100

    added example for test M5CoreS3 on WiFi raw

---
+++ examples/wifiraw-80211tx-m5cores3-sender/wifiraw-80211tx-m5cores3-sender.cpp
@@ -0,0 +1,90 @@
+/**************************************************
+ * ESPNowCam video Transmitter
+ * by @hpsaturn Copyright (C) 2024
+ * This file is part ESPNowCam project:
+ * https://github.com/hpsaturn/ESPNowCam
+**************************************************/
+
+#include <M5CoreS3.h>
+#include "esp_camera.h"
+#include "ESPNowCam.h"
+#include "Utils.h"
+
+WiFiRawComm wifiRaw;
+ESPNowCam radio(&wifiRaw);
+
+int32_t dw, dh;
+
+static void drawFPS() {
+  static uint_least64_t timeStamp = 0;
+  frame++;
+  if (millis() - timeStamp > 1000) {
+    timeStamp = millis();
+    char bf [10];
+    sprintf(bf,"FPS: %i",frame);
+    CoreS3.Display.clear();
+    CoreS3.Display.drawString(String(bf), dw / 2, dh / 2);
+    // Serial.printf("%s %2d FPS\r\n",msg, frame);
+    frame = 0;
+  } 
+}
+
+void processFrame() {
+  if (CoreS3.Camera.get()) {
+    CoreS3.Display.pushImage(0, 0, dw, dh, (uint16_t *)CoreS3.Camera.fb->buf);
+    uint8_t *out_jpg = NULL;
+    size_t out_jpg_len = 0;
+    frame2jpg(CoreS3.Camera.fb, 18, &out_jpg, &out_jpg_len);
+    // CoreS3.Display.drawJpg(out_jpg, out_jpg_len, 0, 0, dw, dh);
+    radio.sendData(out_jpg, out_jpg_len);
+    // Serial.printf("format: %i bufferlen: %i\r\n",CoreS3.Camera.fb->format, CoreS3.Camera.fb->len);
+    // radio.sendData(CoreS3.Camera.fb->buf, CoreS3.Camera.fb->len);
+    // vTaskDelay(5);
+    printFPS("CAM:");
+    free(out_jpg);
+    CoreS3.Camera.free();
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  auto cfg = M5.config();
+  CoreS3.begin(cfg);
+  CoreS3.Display.setTextColor(GREEN);
+  CoreS3.Display.setTextDatum(middle_center);
+  CoreS3.Display.setFont(&fonts::Orbitron_Light_24);
+  CoreS3.Display.setTextSize(1);
+
+  if(psramFound()){
+    size_t psram_size = esp_spiram_get_size() / 1048576;
+    Serial.printf("PSRAM size: %dMb\r\n", psram_size);
+  }
+
+  const uint8_t macRecv[6] = {0xB8,0xF0,0x09,0xC6,0x0E,0xCC};
+  radio.setTarget(macRecv);
+  radio.setChannel(6);
+  
+  if (radio.init(512)) {  // chunk size in bytes (beta feature)
+    Serial.println("ESPNowCam Init Success");
+  }
+
+  dw = CoreS3.Display.width();
+  dh = CoreS3.Display.height();
+
+  if (!CoreS3.Camera.begin()) {
+    CoreS3.Display.drawString("Camera Init Fail", dw / 2, dh / 2);
+  }
+  CoreS3.Display.drawString("Camera Init Success", dw / 2, dh / 2);
+
+  // CoreS3.Camera.config->fb_count = 2;
+  // CoreS3.Camera.config->fb_location = CAMERA_FB_IN_DRAM;
+  // CoreS3.Camera.config->pixel_format = PIXFORMAT_GRAYSCALE;
+  CoreS3.Camera.sensor->set_framesize(CoreS3.Camera.sensor, FRAMESIZE_QVGA);
+
+  delay(500);
+}
+
+void loop() {
+  processFrame();
+}
