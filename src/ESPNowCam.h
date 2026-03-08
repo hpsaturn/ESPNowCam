5650bc6337d5d23fc452d7fd6139e9c556b405ba
Hpsaturn <hpsaturn@gmail.com>
2026-03-08 22:26:17 +0100

    v0.2.1 big payload issues fixed. Improved to ~14FPS on M5CoreS3

--- src/ESPNowCam.h
+++ src/ESPNowCam.h
@@ -24,8 +24,8 @@ extern "C" {
 typedef void (*RecvCb)(uint32_t lenght);
 }
 
-#define ENC_VERSION "0.2.0"
-#define ENC_REVISION 083
+#define ENC_VERSION "0.2.1"
+#define ENC_REVISION 084
 
 // Maximum data length (can be overridden by implementations)
 #ifndef COMM_MAX_DATA_LEN
