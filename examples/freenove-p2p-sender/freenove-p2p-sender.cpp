d6164e26e6dccd51bdb498952d9ed55bd06fe70b
Hpsaturn <hpsaturn@gmail.com>
2026-09-15 18:53:03 +0200

    fixed espressif version. Seeems that is not working for lastest one

--- examples/freenove-p2p-sender/freenove-p2p-sender.cpp
+++ examples/freenove-p2p-sender/freenove-p2p-sender.cpp
@@ -51,7 +51,7 @@ void setup() {
 
   // You are able to change the Camera config E.g:
   // Camera.config.fb_count = 2;
-  // Camera.config.frame_size = FRAMESIZE_QQVGA;
+  Camera.config.frame_size = FRAMESIZE_QQVGA;
   
   if (Camera.begin()) {
     Serial.println("Camera Init Success");
